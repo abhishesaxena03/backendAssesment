4b0c3f15c039ac10eef409f26dbe2c26e2e9ed72
abhishesaxena03 <98497732+abhishesaxena03@users.noreply.github.com>
2022-02-09 09:30:37 -0800

    Update LibraryController.java

--- src/main/java/com/galvanize/tmo/paspringstarter/LibraryController.java
+++ src/main/java/com/galvanize/tmo/paspringstarter/LibraryController.java
@@ -1,16 +1,11 @@
 package com.galvanize.tmo.paspringstarter;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import java.awt.print.Book;
+import java.util.*;
 @RestController
 public class LibraryController {
-
     ArrayList<Books> bookList = new ArrayList<Books>();
-
     @PostMapping("/api/books")
     @ResponseStatus(HttpStatus.CREATED)
     public Books addBooks(@RequestBody Books book) {
@@ -18,13 +13,19 @@ public class LibraryController {
 		bookList.add(book);
         return book;
     }
-
     @GetMapping("/api/books")
     @ResponseStatus(HttpStatus.OK)
-    public List<Books> readBooks() {
-        return bookList;
+    public Map<String, List<Books>> readBooks() {
+        Collections.sort(bookList, new Comparator<Books>() {
+            @Override
+            public int compare(Books o1, Books o2) {
+                return o1.getTitle().compareTo(o2.getTitle());
+            }
+        });
+        Map<String, List<Books>> books = new HashMap<String, List<Books>>();
+        books.put("books", bookList);
+        return books;
     }
-
     @DeleteMapping("/api/books")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void removeBooks() {
